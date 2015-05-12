1f36cee7b583b4a13bd7f50e0db0cb8e8f3bc748
Richard Kellner <richard.kellner@erigones.com>
2015-05-13 00:07:50 +0200

    Moving towards 0.7.0a version

--- ludolph/__init__.py
+++ ludolph/__init__.py
@@ -1,10 +1,10 @@
 """
 Ludolph: Monitoring Jabber Bot
-Version: 0.6.0
+Version: 0.7.0a
 Homepage: https://github.com/erigones/Ludolph
 Copyright (C) 2012-2015 Erigones, s. r. o.
 
 See the LICENSE file for copying permission.
 """
 
-__version__ = '0.6.0'
+__version__ = '0.7.0a'
