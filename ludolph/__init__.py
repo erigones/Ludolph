d2e1b7741e404cd05f15d244da7cb89ec876b9c1
Daniel Kontsek <daniel@kontsek.sk>
2016-02-17 09:55:51 +0100

    Fixed copyright year

--- ludolph/__init__.py
+++ ludolph/__init__.py
@@ -2,7 +2,7 @@
 Ludolph: Monitoring Jabber Bot
 Version: 0.8.0a
 Homepage: https://github.com/erigones/Ludolph
-Copyright (C) 2012-2015 Erigones, s. r. o.
+Copyright (C) 2012-2016 Erigones, s. r. o.
 
 See the LICENSE file for copying permission.
 """
