91328d4670aa94b103ec48296f0b96527f567c3c
Daniel Kontsek <daniel@kontsek.sk>
2016-07-20 21:35:49 +0200

    Initializing development of version 0.9.0

--- ludolph/__init__.py
+++ ludolph/__init__.py
@@ -1,10 +1,10 @@
 """
 Ludolph: Monitoring Jabber Bot
-Version: 0.8.0
+Version: 0.9.0
 Homepage: https://github.com/erigones/Ludolph
 Copyright (C) 2012-2016 Erigones, s. r. o.
 
 See the LICENSE file for copying permission.
 """
 
-__version__ = '0.8.0'
+__version__ = '0.9.0'
