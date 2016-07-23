e8c319825ec11b70fcfecb038902494d16fca031
Daniel Kontsek <daniel@kontsek.sk>
2016-07-23 13:51:06 +0200

    Updated copyright year

--- ludolph/plugins/__init__.py
+++ ludolph/plugins/__init__.py
@@ -1,6 +1,6 @@
 """
 Ludolph: Monitoring Jabber bot
-Copyright (C) 2012-2015 Erigones, s. r. o.
+Copyright (C) 2012-2016 Erigones, s. r. o.
 This file is part of Ludolph.
 
 See the file LICENSE for copying permission.
